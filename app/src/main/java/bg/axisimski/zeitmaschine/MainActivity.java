dad09e06e52dceed403377ea6a9704b627a28b14
Alexander Simeonovski <33306491+axisimski@users.noreply.github.com>
2018-10-03 17:53:09 -0500

    wwii activity created

--- app/src/main/java/bg/axisimski/zeitmaschine/MainActivity.java
+++ app/src/main/java/bg/axisimski/zeitmaschine/MainActivity.java
@@ -2,15 +2,53 @@ package bg.axisimski.zeitmaschine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-     
+     Button mars_btn, wwii_btn, jurassic_btn, other_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mars_btn=findViewById(R.id.mars_btn);
+        jurassic_btn=findViewById(R.id.jurassic_btn);
+        wwii_btn=findViewById(R.id.ww2_btn);
+        other_btn=findViewById(R.id.other_btn);
+
+
+        mars_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        jurassic_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        wwii_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        other_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/bg/axisimski/zeitmaschine/WWII_Act.java
@@ -0,0 +1,13 @@
+package bg.axisimski.zeitmaschine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WWII_Act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wwii_);
+    }
+}
