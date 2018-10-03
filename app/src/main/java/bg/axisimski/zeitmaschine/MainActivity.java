ab271272c3e62cc2609597b5365b7a77f555c815
Alexander Simeonovski <33306491+axisimski@users.noreply.github.com>
2018-10-03 18:40:06 -0500

    set up wwii

--- app/src/main/java/bg/axisimski/zeitmaschine/MainActivity.java
+++ app/src/main/java/bg/axisimski/zeitmaschine/MainActivity.java
@@ -1,5 +1,6 @@
 package bg.axisimski.zeitmaschine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -38,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
         wwii_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Intent intent =new Intent(MainActivity.this, WWII_Act.class);
+                startActivity(intent);
 
             }
         });
--- app/src/main/java/bg/axisimski/zeitmaschine/WWII_Act.java
+++ app/src/main/java/bg/axisimski/zeitmaschine/WWII_Act.java
@@ -1,13 +1,18 @@
 package bg.axisimski.zeitmaschine;
 
+import android.icu.util.Output;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class WWII_Act extends AppCompatActivity {
 
     private Button forward_btn, back_btn, left_btn, right_btn, ok_btn, no_btn;
+    public TextView output_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +25,20 @@ public class WWII_Act extends AppCompatActivity {
         right_btn=findViewById(R.id.right_btn);
         ok_btn=findViewById(R.id.ok_btn);
         no_btn=findViewById(R.id.no_btn);
+        output_tv=findViewById(R.id.output_tv);
+
+        output_tv.setText("Welcome to 1930's Germany, the country is rapidly militarizing." +
+                "You are a poor class jewish person and with only 50 marks to your name" +
+                "What would you like to do? Press 'Right' to immigrate east to Poland, 'Left to immigrate to the US" +
+                "or ok to stay put for now ");
 
         setButtons();
 
-    }
-
-    public void run (char c){
 
 
     }
+
+    public void run (char c){ }
 
 
     private void setButtons(){
